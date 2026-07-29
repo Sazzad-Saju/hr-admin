811b340044d1b833d6ce690c6a9a78fd849d4feb
Sazzad-Saju <saju.cse.hstu@gmail.com>
2026-07-29 15:51:04 +0600

    Add: full sidebar + basic component header

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -22,6 +22,25 @@ import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
+const Item = ({ title, to, icon, selected, setSelected }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return (
+        <MenuItem
+            component={<Link to={to} />}
+            active={selected === title}
+            onClick={() => setSelected(title)}
+            icon={icon}
+            style={{
+                color: colors.grey[100],
+            }}
+        >
+            <Typography>{title}</Typography>
+        </MenuItem>
+    );
+};
+
 const Sidebar = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
@@ -116,10 +135,10 @@ const Sidebar = () => {
                                 >
                                     Joshef Smith
                                 </Typography>
-                                <Typography variant="h5" 
-                                sx={{
-                                    color: colors.greenAccent[500],
-                                }}>
+                                <Typography variant="h5"
+                                    sx={{
+                                        color: colors.greenAccent[500],
+                                    }}>
                                     Sattar Admin
                                 </Typography>
                             </Box>
@@ -127,94 +146,102 @@ const Sidebar = () => {
                     )}
 
                     {/* MENU ITEMS */}
-                    <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+                    <Box sx={{
+                        pl: isCollapsed ? 0 : "10%",
+                    }}>
+                        <Item
+                            title="Dashboard"
+                            to="/"
+                            icon={<HomeOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+
+                        <Typography variant="h6" color={colors.grey[300]} sx={{ m: "15px 0 5px 20px" }}>
+                            Data
+                        </Typography>
+                        <Item
+                            title="Manage Team"
+                            to="/team"
+                            icon={<PeopleOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+                        <Item
+                            title="Contacts Information"
+                            to="/contacts"
+                            icon={<ContactsOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+                        <Item
+                            title="Invoices Balances"
+                            to="/invoices"
+                            icon={<ReceiptOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+
+                        <Typography variant="h6" color={colors.grey[300]} sx={{ m: "15px 0 5px 20px" }}>
+                            Pages
+                        </Typography>
+                        <Item
+                            title="Profile Form"
+                            to="/form"
+                            icon={<PersonOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+
+                        />
+                        <Item
+                            title="Calendar"
+                            to="/calendar"
+                            icon={<CalendarTodayOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+                        <Item
+                            title="FAQ Page"
+                            to="/faq"
+                            icon={<HelpOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+                        <Typography variant="h6" color={colors.grey[300]} sx={{ m: "15px 0 5px 20px" }}>
+                            Charts
+                        </Typography>
+                        <Item
+                            title="Bar Chart"
+                            to="/bar"
+                            icon={<BarChartOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+                        <Item
+                            title="Pie Chart"
+                            to="/pie"
+                            icon={<PieChartOutlineOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+                        <Item
+                            title="Line Chart"
+                            to="/line"
+                            icon={<TimelineOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+                        <Item
+                            title="Geography Chart"
+                            to="/geography"
+                            icon={<MapOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
                     </Box>
                 </Menu>
             </ProSidebar>
         </Box>
-
-
-        // <Box
-        //     sx={{
-        //         "& .pro-sidebar-inner": {
-        //             background: `${colors.primary[400]} !important`,
-        //         },
-        //         "& .ps-icon-wrapper": {
-        //             backgroundColor: "transparent !important",
-        //         },
-        //         "& .ps-inner-item": {
-        //             padding: "5px 35px 5px 20px !important",
-        //         },
-        //         "& .ps-inner-item:hover": {
-        //             color: "#868dfb !important",
-        //         },
-        //         "& .ps-menu-item.active": {
-        //             color: "#6870fa !important",
-        //         },
-        //     }}
-        // >
-        //     <ProSidebar collapsed={isCollapsed}>
-        //         <Menu iconShape="square">
-        //             {/* LOGO AND MENU ICON */}
-        //             <MenuItem
-        //                 onClick={() => setIsCollapsed(!isCollapsed)}
-        //                 icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
-        //                 style={{
-        //                     margin: "10px 0 20px 0",
-        //                     color: colors.grey[100],
-        //                 }}
-        //             > 
-        //             {!isCollapsed && (
-        //                 <Box sx={{
-        //                     display: "flex",
-        //                     justifyContent: "space-between",
-        //                     alignItems: "center",
-        //                     ml: "15px",
-        //                 }}>
-        //                     <Typography variant="h3" color={colors.grey[100]}>
-        //                         ADMINS
-        //                     </Typography>
-        //                     <IconButton>
-        //                         <MenuOutlinedIcon />
-        //                     </IconButton>
-        //                 </Box>
-        //             )}
-        //             </MenuItem>
-
-        //             {/* USER */}
-        //             {!isCollapsed && (
-        //                 <Box sx={{mb: "25px"}}>
-        //                     <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
-        //                         <img
-        //                             alt="profile-user"
-        //                             width="100px"
-        //                             height="100px"
-        //                             src={`../../assets/anonymous.png`}
-        //                             style={{ cursor: "pointer", borderRadius: "50%" }}
-        //                         />
-        //                     </Box>
-        //                     <Box sx={{ textAlign: "center" }}>
-        //                         <Typography
-        //                             variant="h2"
-        //                             color={colors.grey[100]}
-        //                             fontWeight="bold"
-        //                             sx={{ m: "10px 0 0 0" }}
-        //                         >
-        //                             Joshef Smith
-        //                         </Typography>
-        //                         <Typography variant="h5" color={colors.greenAccent[500]}>
-        //                             Sattar Admin
-        //                         </Typography>
-        //                     </Box>
-        //                 </Box>
-        //             )}
-
-        //             {/* MENU ITEMS */}
-        //             <Box paddingLeft={isCollapsed ? undefined : "10%"}>
-        //             </Box>
-        //         </Menu>
-        //     </ProSidebar>
-        // </Box>
     );
 };
 
